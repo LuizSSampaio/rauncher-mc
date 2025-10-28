9f1d2f119964c4d3bca8a81f6540fa5f09081b0d
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2025-10-28 18:50:05 -0300

    feat(rc-core): Add workspace attributes

--- crates/rc-core/src/lib.rs
+++ crates/rc-core/src/lib.rs
@@ -1,14 +1,4 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+#[derive(Debug, Default, Clone, PartialEq, Eq)]
+pub struct RCCore {}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+impl RCCore {}
