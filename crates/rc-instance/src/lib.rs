388287633ed1238eb3526f4971d47b272786bc91
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2025-11-04 17:58:46 -0300

    feat(rc-instance): Add Minecraft module and version manifest

--- crates/rc-instance/src/lib.rs
+++ crates/rc-instance/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod config;
 mod instance;
 mod manager;
+mod minecraft;
 
 pub use instance::Instance;
 pub use manager::{InstanceManager, InstanceManagerError};
---
+++ crates/rc-instance/src/minecraft/version_manifest.rs
@@ -0,0 +1,2 @@
+const VERSION_MANIFEST_DOWNLOAD_URL: &str =
+    "https://launchermeta.mojang.com/mc/game/version_manifest_v2.json";
